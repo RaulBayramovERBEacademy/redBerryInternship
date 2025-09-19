79baf6c00c60574bca5ea5fdde7006e8e0fec21a
erbe <erbeacademy1@gmail.com>
2025-09-20 01:58:01 +0400

    update registration page styles and functionality

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,17 @@
 let profilePic = document.querySelector(".profile-image");
-let inputFile = document.querySelector("input-file");
+let inputFile = document.querySelector("#input-file");
+let profilePicStyle = getComputedStyle(profilePic);
+
 inputFile.onchange = function () {
   profilePic.src = URL.createObjectURL(inputFile.files[0]);
+  profilePic.style.width = "100px";
+  profilePic.style.height = "100px";
+  profilePic.style.objectFit = "cover";
+  profilePic.style.borderRadius = "50%";
 };
+
+document.querySelector(".remove-img").addEventListener("click", function () {
+  profilePic.src = "images/camera.png";
+  profilePic.style.width = "20px";
+  profilePic.style.height = "20px";
+});
