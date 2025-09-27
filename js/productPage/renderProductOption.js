47278f34ea23204f26f826a18122ab4647de9b6e
erbe <erbeacademy1@gmail.com>
2025-09-27 18:05:58 +0400

    feat(product-options): add styles and interactive features
    - Applied styles to ProductOptions component
    - Implemented color selection with dynamic current image update
    - Added quantity and size selection functionality

--- js/productPage/renderProductOption.js
+++ js/productPage/renderProductOption.js
@@ -1,19 +1,41 @@
 export function renderProductOption(product) {
+  const COLORS = {
+    Beige: "#F5F5DC",
+    "Navy Blue": "#000080",
+    Red: "red",
+    Blue: "blue",
+    Green: "green",
+    Multi: "rainbow",
+  };
+
   let productOptionsContainerHTML = `
     <div class="title-price-container">
           <h1>${product.name}</h1>
-          <p>${product.price}</p>
+          <p>$ ${product.price}</p>
         </div>
-        <div class="product-options-container">
+        <div class="product-options">
           <div class="avaliable-colors-container">
             <p>Color: ${product.color}</p>
             <div class="avaliable-colors">
               ${product.available_colors
-                .map(
-                  (color) =>
-                    `<div style="background-color:${color.toLowerCase()}" class="color"></div>`
-                )
+                .map((color, index) => {
+                  let style;
+
+                  if (color === "Multi") {
+                    style =
+                      "background: linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet)";
+                  } else if (COLORS[color]) {
+                    style = `background-color: ${COLORS[color]}`;
+                  } else {
+                    style = `background-color: ${color.toLowerCase()}`;
+                  }
+
+                  return `<div class="color ${
+                    index === 0 ? "selected" : ""
+                  }" data-color="${color}" style="${style}"></div>`;
+                })
                 .join("")}
+
 
             </div>
           </div>
@@ -21,8 +43,10 @@ export function renderProductOption(product) {
             <p>Size: ${product.size}</p>
             <div class="avaliable-sizes">
             ${product.available_sizes
-              .map((size) => {
-                return `<div class = "size-${size}">${size}</div>`;
+              .map((size, index) => {
+                return `<div class = "size ${
+                  index === 0 ? "selected" : ""
+                }" data-size="${size}">${size}</div>`;
               })
               .join("")}
             </div>
@@ -30,7 +54,7 @@ export function renderProductOption(product) {
           <div class="quantity-container">
             <p>Quantity</p>
             <div class="quantity">
-              <label for="qty">Quantity</label>
+      
                 <select id="qty" name="qty">
                   ${Array.from(
                     { length: product.quantity },
@@ -57,9 +81,15 @@ export function renderProductOption(product) {
           </svg>
           <p>Add to cart</p>
         </div>
+        <div style="display: block;
+             width: 100%;
+             height: 2px;
+             background-color: #E1DFE1;" 
+            class="divider"></div>
+
         <div class="product-details-container">
-          <div class="product-deatils title">
-            <p>Detail:</p>
+          <div class="product-deatils-title">
+            <p>Details:</p>
             <img class="brand-image" src="${
               product.brand.image
             }" alt="product-brand-image" />
@@ -70,6 +100,33 @@ export function renderProductOption(product) {
           </div>
         </div>
   `;
+  const currentImg = document.querySelector(".product-selected-img");
   document.querySelector(".product-options-container").innerHTML =
     productOptionsContainerHTML;
+  document.querySelector(".avaliable-colors").addEventListener("click", (e) => {
+    if (e.target.classList.contains("color")) {
+      document
+        .querySelectorAll(".color")
+        .forEach((c) => c.classList.remove("selected"));
+      e.target.classList.add("selected");
+      const selectedColor = e.target.dataset.color;
+
+      if (selectedColor === "Multi") {
+        currentImg.src = product.images[product.images.length - 1];
+      } else {
+        const colors = Array.from(document.querySelectorAll(".color"));
+        const index = colors.indexOf(e.target);
+        currentImg.src = product.images[index] || product.cover_image;
+      }
+    }
+  });
+  document.querySelector(".avaliable-sizes").addEventListener("click", (e) => {
+    if (e.target.classList.contains("size")) {
+      document
+        .querySelectorAll(".size")
+        .forEach((c) => c.classList.remove("selected"));
+      e.target.classList.add("selected");
+      const selectedColor = e.target.dataset.size;
+    }
+  });
 }
